0f3b64cc60dae3f3390041e5fe5ffa5defa9e1ac
Yuto Suzuki <y.suzuki.sys21@morijyobi.ac.jp>
2022-03-10 10:23:05 +0900

    2022/3/10  練習問題2
    練習問題2を解いて提出する。
    努力と協力をして完成へと近づける。
    努力は目に見えるものではない。
    力となり、支えてくれるものである。

--- pra2.java
+++ pra2.java
@@ -1,5 +1,7 @@
 public class main {
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		for(int cnt = 0 ; cnt < 100 ; cnt++){
+			System.out.println("Hello World");
+		}
 	}
 }
